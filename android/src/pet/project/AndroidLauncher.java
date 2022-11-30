ee4697804e9733a69194c2f13d30206f3e31af73
ldinkaofficial <klassnyjartem8@gmail.com>
2022-11-30 23:56:19 +0300

    Initial commit

---
+++ android/src/pet/project/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pet.project;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pet.project.SurvIVEit;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SurvIVEit(), config);
+	}
+}
